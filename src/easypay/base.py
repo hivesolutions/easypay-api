b60c8ffaf1cca6b05511a2a5457041c4cf589085
joamag <joamag@gmail.com>
2014-05-16 16:32:50 +0100

    new method and url reference

--- src/easypay/base.py
+++ src/easypay/base.py
@@ -146,7 +146,7 @@ class Api(
         if not status == "ok0": raise errors.ApiError(message)
         return result
 
-    def build(self, headers, kwargs):
+    def build(self, method, url, headers, kwargs):
         appier.Api.build(self, headers, kwargs)
         if self.cin: kwargs["ep_cin"] = self.cin
         if self.username: kwargs["ep_user"] = self.username
