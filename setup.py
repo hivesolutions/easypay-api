f418502095489542f6e4c3495dda183de888edc9
joamag <joamag@gmail.com>
2015-10-20 11:06:45 +0100

    new version

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ import setuptools
 
 setuptools.setup(
     name = "easypay_api",
-    version = "0.1.9",
+    version = "0.1.10",
     author = "Hive Solutions Lda.",
     author_email = "development@hive.pt",
     description = "Easypay API Client",
