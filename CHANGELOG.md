d50fa7618119c9b3befc866d04d82857547cb93a
João Magalhães <joamag@gmail.com>
2025-01-03 12:54:48 +0000

    version: 0.3.7

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -17,6 +17,12 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ### Fixed
 
+*
+
+## [0.3.7] - 2025-01-03
+
+### Fixed
+
 * Serious issue with cancel payment
 
 ## [0.3.6] - 2024-12-31
--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ import setuptools
 
 setuptools.setup(
     name="easypay-api",
-    version="0.3.6",
+    version="0.3.7",
     author="Hive Solutions Lda.",
     author_email="development@hive.pt",
     description="Easypay API Client",
